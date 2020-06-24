886a148fffbf0dd3676fba1845666c7b00a59b0b
slimchin <suchinchuendang@gmail.com>
2020-06-24 21:19:35 +0700

    1
    1

--- TemplateWeb/CSMS/ConsentManagement/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConsentManagement.MvcApplication" Language="C#" %>
--- Test/GetTokenTable.sql
+++
@@ -1,7 +0,0 @@
-select * from MVC.JWT_ClientRegisterInfo
-select * from MVC.JWT_ClientRegisterInfo_Log
-
- 
-
-select * from MVC.JWT_SecretKeyInfo
-select * from MVC.JWT_SecretKeyInfo_Log
